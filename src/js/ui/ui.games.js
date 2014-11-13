76211d3885465fb3dc27ee59ef6e64108b7a1c84
Julien <hello@julienfouquet.fr>
2014-11-13 20:58:18 +0100

    Proto commited
    Attention, les pages HTML sont à la racine, pas de Grunt pour le proto.
    Appelle-moi si besoin ;)

--- src/js/ui/ui.games.js
+++ src/js/ui/ui.games.js
@@ -1,4 +1,4 @@
-var uiGames = function(){	
+$(document).ready(function(){
 
 	function doNothing(event){
 		event.preventDefault();
@@ -7,9 +7,9 @@ var uiGames = function(){
 		
 	var urlParts = window.location.href.split('/'),
 			currentPage = urlParts[urlParts.length - 1];
-	history.pushState(null, null, 'games');
+	//history.pushState(null, null, 'games');
 	// Navigation
-	$('body').on('click','a[data-target]',function(e){
+	$('body').on('click','[data-target]',function(e){
 		doNothing(e);
 		var id = '#' + $(this).attr('data-target'),
 				tX = -100 * $(id).index();
@@ -31,7 +31,29 @@ var uiGames = function(){
 	// OnPopState : used on history navigation
 	$(window).on('popstate', function () {
 			urlParts = window.location.href.split('/');
-			$('a[data-target=' + urlParts[urlParts.length - 1] + ']:eq(0)').click();
+			//$('a[data-target=' + urlParts[urlParts.length - 1] + ']:eq(0)').click();
 	});
-};
-module.exports = uiGames;
+
+	// Action on game
+	$('#games .games').on('click','li',function(e){
+		if(e.target.tagName.toLowerCase() != 'a')
+			$(this).find('.action').show();
+	});
+	$('#games .games').on('click','.action button',function(e){
+		doNothing(e);
+		$(this).closest('.action').hide();
+	});
+
+	// Action on friends
+	$('.friends').on('click','.more',function(e){
+		$(this).closest('li').hide(150,function(){$(this).remove();});
+	});
+	$('#games .games').on('click','.action a',function(e){
+		doNothing(e);
+		$(this).closest('.action').hide();
+	});
+
+	// Give mask to fields
+  $('#gameDate').mask('00/00/0000', {placeholder: "__/__/____"});
+  $('#gameHour').mask('00:00', {placeholder: "__:__"});
+});
